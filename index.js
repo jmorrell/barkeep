ea43fb21425eab74e52eb97154a4d7ce66b094ec
Hunter Loftis <hunter@recurly.com>
2014-03-31 14:19:11 -0700

    exposed as a module

---
+++ index.js
@@ -0,0 +1,54 @@
+var tinylr = require('tiny-lr');
+var express = require('express');
+var Gaze = require('gaze').Gaze;
+var cors = require('cors');
+var path = require('path');
+
+var inject = require('./src/inject.js');
+var urlCallback = require('./src/url-callback.js');
+
+function Barkeep(root) {
+  this._addToGaze = this._addToGaze.bind(this);
+  this.root = root;
+  this.server = tinylr();
+  this.app = express();
+  this.gaze = new Gaze(null, { debounceDelay: 200 });
+  this.watchedFiles = [];
+}
+
+module.exports = function barkeep(root) {
+  return new Barkeep(root);
+};
+
+Barkeep.prototype.listen = function(staticPort, lrPort, done) {
+  var self = this;
+  staticPort = staticPort || 9000;
+  lrPort = lrPort || 35729;
+
+  this.server.listen(lrPort);
+
+  this.gaze.on('changed', function(filepath) {
+    self.server.changed({ body: { files: filepath.replace(self.root, '') } });
+  });
+
+  return this.app
+    .use(cors())
+    .use(inject('http://localhost:' + lrPort + '/livereload.js'))
+    .use(urlCallback(this._addToGaze))
+    .use(express.static(this.root))
+    .listen(staticPort, done);
+};
+
+Barkeep.prototype._addToGaze = function(url) {
+  // If it's a directory, add index.html to the end
+  if (/\/$/.test(url)) {
+    url += 'index.html';
+  }
+
+  if (this.watchedFiles.indexOf(url) === -1) {
+    this.watchedFiles.push(url);
+    this.gaze.add(path.join(this.root, url));
+    console.log('Added:', url, 'to list of watched files');
+  }
+};
+
